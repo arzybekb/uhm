32918dcb275cee42bf2ee25b50c8c5f545a0af8e
arzybekb <arzybekb@gmail.com>
2024-08-08 14:33:09 -0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Button } from "@mui/material";
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { Button, Tab } from "@mui/material";
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Tabs, { tabsClasses } from "@mui/material/Tabs";
+import { Typography } from "@mui/material";
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-const tg = window.Telegram.WebApp;
 function App() {
+  // @ts-ignore
+  const tg = window.Telegram.WebApp;
+  console.log(tg);
+  
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   const onClose = () => {
     tg.close();
   };
@@ -14,6 +24,28 @@ function App() {
   }, []);
   return (
     <div>
+      <Typography align="center" color="black">
+        Hi {tg?.initDataUnsafe?.user?.username}
+      </Typography>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="scrollable"
+        scrollButtons
+        sx={{
+          [`& .${tabsClasses.scrollButtons}`]: {
+            "&.Mui-disabled": { opacity: 0.3 },
+          },
+        }}
+      >
+        <Tab label="Item One" />
+        <Tab label="Item Two" />
+        <Tab label="Item Three" />
+        <Tab label="Item Four" />
+        <Tab label="Item Five" />
+        <Tab label="Item Six" />
+        <Tab label="Item Seven" />
+      </Tabs>
       <Button variant="contained" onClick={onClose}>
         Close
       </Button>
